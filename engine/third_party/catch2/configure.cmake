9eca6ae7bfff118be93860d05d1b6ba134cf7a1d
oric432 <oric498@gmail.com>
2026-07-20 13:09:14 +0300

    chore: update Catch2 dependency to version 3.15.2 and clean up build configuration

--- engine/third_party/catch2/configure.cmake
+++ engine/third_party/catch2/configure.cmake
@@ -1,8 +1,11 @@
-FetchContent_MakeAvailable(Catch2)
+add_library(tp_catch2 STATIC
+    ${CMAKE_CURRENT_LIST_DIR}/catch_amalgamated.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/catch_main.cpp
+)
 
-# Propagate Catch2's CMake module path (for include(Catch)) to the parent scope
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${catch2_SOURCE_DIR}/extras" PARENT_SCOPE)
+target_include_directories(tp_catch2 SYSTEM PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}
+    ${CMAKE_CURRENT_LIST_DIR}/include
+)
 
-add_library(tp_catch2 INTERFACE)
 add_library(third_party::catch2 ALIAS tp_catch2)
-target_link_libraries(tp_catch2 INTERFACE Catch2::Catch2WithMain)
